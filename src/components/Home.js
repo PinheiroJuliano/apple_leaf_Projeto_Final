458cae046e43c6ba76e7113dcfd79ed48d71644a
Juliano <pisjuliano@gmail.com>
2024-07-17 21:05:07 -0300

    inclusão da sessão de destaques com swipper

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import CategoryList from './CategoryList';
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import axios from 'axios'; // Importe axios para fazer requisições HTTP
 import '../styles.css'; // Importando o arquivo de estilo
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [currentCategory, setCurrentCategory] = useState('Hidratação'); // Categoria inicial
+
+  useEffect(() => {
+    // Função para carregar os produtos da API com base na categoria
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(`/produtos.json`);
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+
+    fetchProducts();
+  }, [currentCategory]); // Executa sempre que a categoria atual mudar
+
   return (
     <div>
       <div className="search-bar-container">
@@ -17,7 +38,27 @@ const Home = () => {
           alt="Buscar" 
         />
       </div>
-      <CategoryList />
+      
+      <div className="highlights-section">
+        <h2>Destaques</h2>
+        <Swiper
+            spaceBetween={1} // Espaço entre os slides
+            slidesPerView={3} // Quantidade de slides visíveis ao mesmo tempo
+            navigation
+            pagination={{ clickable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+          >
+            {products.map((product) => (
+              <SwiperSlide key={product._id}>
+                <div className="highlight-item">
+                  <img src={product.image} alt={product.name} />
+                  <p>{product.name}</p>
+                </div>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </div>
     </div>
   );
 };
